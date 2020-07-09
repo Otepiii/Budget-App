d56d6ed4e0fd6281e70c2d3e5ac96df8dd3103f2
joseph Evangelista <oootep123@gmail.com>
2020-07-08 22:35:37 -0400

    cleaned up some code

--- budget.js
+++ budget.js
@@ -1,174 +1,175 @@
 // Html Elements
-const balanceEl = document.querySelector(".balance .value");
-const incomeTotal = document.querySelector(".income-total");
-const outcomeTotal = document.querySelector(".outcome-total");
-const incomeEl = document.querySelector("#income");
-const expenseEl = document.querySelector("#expense");
-const allEl = document.querySelector("#all");
-const incomeList = document.querySelector("#income .list");
-const expenseList = document.querySelector("#expense .list");
-const allList = document.querySelector("#all .list");
+const balanceEl = document.querySelector(".balance .value")
+const incomeTotal = document.querySelector(".income-total")
+const outcomeTotal = document.querySelector(".outcome-total")
+const incomeEl = document.querySelector("#income")
+const expenseEl = document.querySelector("#expense")
+const allEl = document.querySelector("#all")
+const incomeList = document.querySelector("#income .list")
+const expenseList = document.querySelector("#expense .list")
+const allList = document.querySelector("#all .list")
 
 // Toggle Buttons
-const expenseBtn = document.querySelector(".tab1");
-const incomeBtn = document.querySelector(".tab2");
-const allBtn = document.querySelector(".tab3");
+const expenseBtn = document.querySelector(".tab1")
+const incomeBtn = document.querySelector(".tab2")
+const allBtn = document.querySelector(".tab3")
 
 // Expense Input Buttons
-const addExpense = document.querySelector(".add-expense");
-const expenseTitle = document.querySelector("#expense-title-input");
-const expenseAmount = document.querySelector("#expense-amount-input");
+const addExpense = document.querySelector(".add-expense")
+const expenseTitle = document.querySelector("#expense-title-input")
+const expenseAmount = document.querySelector("#expense-amount-input")
 
 // Income Input Buttons
-const addIncome = document.querySelector(".add-income");
-const incomeTitle = document.querySelector("#income-title-input");
-const incomeAmount = document.querySelector("#income-amount-input");
+const addIncome = document.querySelector(".add-income")
+const incomeTitle = document.querySelector("#income-title-input")
+const incomeAmount = document.querySelector("#income-amount-input")
 
 // Variables
-let ENTRY_LIST = [];
+let ENTRY_LIST = []
 let balance = 0,
   income = 0,
-  outcome = 0;
+  outcome = 0
 
 const DELETE = "delete",
-  EDIT = "edit";
+  EDIT = "edit"
 
-  // Look if there is saved data in localstorage
-  ENTRY_LIST = JSON.parse(localStorage.getItem("entry_list")) || [];
-  updateUI();
+// Look if there is saved data in localstorage
+ENTRY_LIST = JSON.parse(localStorage.getItem("entry_list")) || []
+updateUI()
 
 // Event listeners
 expenseBtn.addEventListener("click", function () {
-  show(expenseEl);
-  hide([incomeEl, allEl]);
-  active(expenseBtn);
-  inactive([incomeBtn, allBtn]);
-});
+  show(expenseEl)
+  hide([incomeEl, allEl])
+  active(expenseBtn)
+  inactive([incomeBtn, allBtn])
+})
 
 incomeBtn.addEventListener("click", function () {
-  show(incomeEl);
-  hide([expenseEl, allEl]);
-  active(incomeBtn);
-  inactive([expenseBtn, allBtn]);
-});
+  show(incomeEl)
+  hide([expenseEl, allEl])
+  active(incomeBtn)
+  inactive([expenseBtn, allBtn])
+})
 
 allBtn.addEventListener("click", function () {
-  show(allEl);
-  hide([incomeEl, expenseEl]);
-  active(allBtn);
-  inactive([incomeBtn, expenseBtn]);
-});
+  show(allEl)
+  hide([incomeEl, expenseEl])
+  active(allBtn)
+  inactive([incomeBtn, expenseBtn])
+})
 
 addExpense.addEventListener("click", function () {
   // if title or amount empty will not run
-  if (!expenseTitle.value || !expenseAmount.value) return;
+  if (!expenseTitle.value || !expenseAmount.value) return
 
   let expense = {
     type: "expense",
     title: expenseTitle.value,
     amount: parseFloat(expenseAmount.value),
-  };
-  ENTRY_LIST.push(expense);
+  }
+  ENTRY_LIST.push(expense)
 
-  updateUI();
-  clearInput([expenseTitle, expenseAmount]);
-});
+  updateUI()
+  clearInput([expenseTitle, expenseAmount])
+})
 
 addIncome.addEventListener("click", function () {
   // if title or amount empty will not run
-  if (!incomeTitle.value || !incomeAmount.value) return;
+  if (!incomeTitle.value || !incomeAmount.value) return
 
   let income = {
     type: "income",
     title: incomeTitle.value,
     amount: parseFloat(incomeAmount.value),
-  };
-  ENTRY_LIST.push(income);
+  }
+  ENTRY_LIST.push(income)
 
-  updateUI();
-  clearInput([incomeTitle, incomeAmount]);
-});
+  updateUI()
+  clearInput([incomeTitle, incomeAmount])
+})
 
-incomeList.addEventListener("click", deleteOrEdit);
-expenseList.addEventListener("click", deleteOrEdit);
-allList.addEventListener("click", deleteOrEdit);
+incomeList.addEventListener("click", deleteOrEdit)
+expenseList.addEventListener("click", deleteOrEdit)
+allList.addEventListener("click", deleteOrEdit)
 
 // Helpers
 
-function deleteOrEdit(event){
-  const targetBtn = event.target;
+function deleteOrEdit(event) {
+  const targetBtn = event.target
 
-  const entry = targetBtn.parentNode;
+  const entry = targetBtn.parentNode
 
-  if( targetBtn.id == DELETE){
-    deleteEntry(entry);
-  }else if (targetBtn.id == EDIT){
-    editEntry(entry);
+  if (targetBtn.id == DELETE) {
+    deleteEntry(entry)
+  } else if (targetBtn.id == EDIT) {
+    editEntry(entry)
   }
 }
 
-function deleteEntry(entry){
-  ENTRY_LIST.splice(entry.id, 1);
+function deleteEntry(entry) {
+  ENTRY_LIST.splice(entry.id, 1)
 
-  updateUI();
+  updateUI()
 }
 
-function editEntry(entry){
-  let ENTRY = ENTRY_LIST[entry.id];
+function editEntry(entry) {
+  let ENTRY = ENTRY_LIST[entry.id]
 
-  if(ENTRY.type == "income"){
-    incomeAmount.value = entry.amount;
-    incomeTitle.value = entry.title;
-  }else if(ENTRY.type == "expense"){
-    expenseAmount.value = entry.amount;
-    expenseTitle.value = entry.title;
+  if (ENTRY.type == "income") {
+    incomeAmount.value = ENTRY.amount
+    incomeTitle.value = ENTRY.title
+  } else if (ENTRY.type == "expense") {
+    expenseAmount.value = ENTRY.amount
+    expenseTitle.value = ENTRY.title
   }
 
-  deleteEntry(entry);
+  deleteEntry(entry)
 }
 
 function show(element) {
-  element.classList.remove("hide");
+  element.classList.remove("hide")
 }
 
 function hide(elements) {
   elements.forEach((element) => {
-    element.classList.add("hide");
-  });
+    element.classList.add("hide")
+  })
 }
 
 function active(element) {
-  element.classList.add("active");
+  element.classList.add("active")
 }
 
 function inactive(elements) {
   elements.forEach((element) => {
-    element.classList.remove("active");
-  });
+    element.classList.remove("active")
+  })
 }
 
 function updateUI() {
-  income = calculateTotal("income", ENTRY_LIST);
-  outcome = calculateTotal("expense", ENTRY_LIST);
-  balance = Math.abs(calculateBalance(income, outcome));
+  income = calculateTotal("income", ENTRY_LIST)
+  outcome = calculateTotal("expense", ENTRY_LIST)
+  balance = Math.abs(calculateBalance(income, outcome))
 
   // Determine sign of balance
-  let sign = (income >= outcome)? "$" : "-$";
+  let sign = income >= outcome ? "$" : "-$"
 
   // update header elements
-  balanceEl.innerHTML= `<small>${sign}</small>${balance}`;
-  incomeTotal.innerHTML = `<small>$</small>${income}`;
-  outcomeTotal.innerHTML = `<small>$</small>${outcome}`;
+  balanceEl.innerHTML = `<small>${sign}</small>${balance}`
+  incomeTotal.innerHTML = `<small>$</small>${income}`
+  outcomeTotal.innerHTML = `<small>$</small>${outcome}`
 
-  clearElement([expenseList,incomeList,allList]);
+  clearElement([expenseList, incomeList, allList])
 
-  ENTRY_LIST.forEach((entry,index) => {
-    if(entry.type == "expense"){
-      showEntry(expenseList,entry.type, entry.title, entry.amount, index)
-    }else if (entry.type == "income"){
-      showEntry(incomeList,entry.type, entry.title, entry.amount, index)
-    } (entry.type == "all")
-      showEntry(allList,entry.type, entry.title, entry.amount, index)
+  ENTRY_LIST.forEach((entry, index) => {
+    if (entry.type == "expense") {
+      showEntry(expenseList, entry.type, entry.title, entry.amount, index)
+    } else if (entry.type == "income") {
+      showEntry(incomeList, entry.type, entry.title, entry.amount, index)
+    }
+    entry.type == "all"
+    showEntry(allList, entry.type, entry.title, entry.amount, index)
   })
 
   updateChart(income, outcome)
@@ -177,41 +178,41 @@ function updateUI() {
   localStorage.setItem("entry_list", JSON.stringify(ENTRY_LIST))
 }
 
-function showEntry(list,type,title,amount,id){
+function showEntry(list, type, title, amount, id) {
   const entry = `<li id="${id}" class="${type}">
                   <div class="entry"> ${title}:$${amount}</div>
                   <i class="far fa-edit" id="edit"></i>
                   <i class="far fa-trash-alt" id="delete"></i>
   </li>`
 
-  const position = "afterbegin";
+  const position = "afterbegin"
 
-  list.insertAdjacentHTML(position,entry)
+  list.insertAdjacentHTML(position, entry)
 }
 
-function clearElement(elements){
-  elements.forEach(element => {
-    element.innerHTML = "";
+function clearElement(elements) {
+  elements.forEach((element) => {
+    element.innerHTML = ""
   })
 }
 
 function calculateTotal(type, list) {
-  let sum = 0;
+  let sum = 0
 
   list.forEach((entry) => {
     if (entry.type == type) {
-      sum += entry.amount;
+      sum += entry.amount
     }
-  });
-  return sum;
+  })
+  return sum
 }
 
 function calculateBalance(income, outcome) {
-  return income - outcome;
+  return income - outcome
 }
 
 function clearInput(inputs) {
   inputs.forEach((input) => {
-    input.value = "";
-  });
+    input.value = ""
+  })
 }
